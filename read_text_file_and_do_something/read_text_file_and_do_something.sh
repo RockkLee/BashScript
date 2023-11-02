86aa8f2deea311fa1f9c9b1f86015673bbc10d12
RockkLee <50697724+RockkLee@users.noreply.github.com>
2023-11-02 13:31:53 +0800

    Update read_text_file_and_do_something.sh

--- read_text_file_and_do_something/read_text_file_and_do_something.sh
+++ read_text_file_and_do_something/read_text_file_and_do_something.sh
@@ -4,8 +4,10 @@
 
 # 讀取command的第一個arg
 filename=$1
-
 input="$filename"
+
+# The -r option in the bash code you provided is used to read the input line from the file in raw mode.
+# This means that the backslash (\) character will not be interpreted as an escape character
 while IFS= read -r line
 do
   echo "$line"
